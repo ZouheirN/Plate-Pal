9ca2de39c1a84d5188a319cf67419ad0161181bb
Zouheir Nakouzi <zouheir2002@gmail.com>
2025-01-04 11:38:36 +0200

    Revert "upgrade to android studio 2024"
    This reverts commit 48bf1270d0e68849d4d5de74e9dd11ca287b1fab.

--- android/app/src/main/kotlin/com/zouheirn/platepal/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.zouheirn.platepal
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/features/settings/hive/settings_box.dart
+++
@@ -1,9 +0,0 @@
-import 'package:hive_flutter/hive_flutter.dart';
-
-class SettingsBox {
-  static Box box = Hive.box('settingsBox');
-
-  static void setDarkMode(bool value) {
-    box.put('darkMode', value);
-  }
-}
